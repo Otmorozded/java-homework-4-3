051182211f2ac44bfff7da4448aca4f051332b1d
Evgeny Vasilev <jony.torrent@gmail.com>
2020-11-28 19:21:17 +0300

    java

--- code/CreditPaymentService.java
+++ code/CreditPaymentService.java
@@ -2,7 +2,7 @@ public class CreditPaymentService {
     public double calculate (double sum, double month, double rate) {
 
         double i= rate / month / 100;
-        double k = (i * Math.pow((1 + i),month) / Math.pow((1 + i),month) - 1);
+        double k = (i * Math.pow((1 + i),month) / (Math.pow((1 + i),month) - 1));
         double A = k * sum;
         return A;
 
--- code/Main2.java
+++ code/Main2.java
@@ -2,8 +2,13 @@ public class Main2 {
     public static void main(String[] args) {
 
         CreditPaymentService service = new CreditPaymentService();
-        double A = service.calculate(100000, 12,9.99);
+        double A = service.calculate(1000000, 12,9.99);
         System.out.println(A);
+        double A1 = service.calculate(1000000,24, 9.99);
+        System.out.println(A1);
+        double A2 = service.calculate(1000000,36,9.99);
+        System.out.println(A2);
+
 
     }
 }
